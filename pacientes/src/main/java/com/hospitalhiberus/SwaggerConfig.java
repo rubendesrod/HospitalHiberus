a28a1822ed183dc7d05456fb3b35ed42ca254a8a
Rubén Descalzo Rodríguez <rdescalzo@hiberus.com>
2024-11-12 16:47:15 +0100

    Falla en la documentacion visual de Swagger

--- pacientes/src/main/java/com/hospitalhiberus/SwaggerConfig.java
+++ pacientes/src/main/java/com/hospitalhiberus/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.hospitalhiberus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -14,11 +15,11 @@ public class SwaggerConfig {
 
     private static final String TITLE = "Pacientes API";
     private static final String DESCRIPTION = "API para gestionar los pacientes";
-    private static final String BASE_PACKAGE = "introduccion.api";
+    private static final String BASE_PACKAGE = "com.hospitalhiberus.controller";
     private static final String VERSION = "v1";
 
     @Bean
-    public Docket swagger(){
+    public Docket swagger() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
